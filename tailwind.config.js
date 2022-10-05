248ebbdae118339d382c3fec20734bf6c2d5fb50
Victor Ribeiro <devjvribeiro@gmail.com>
2022-10-05 03:13:48 -0300

    fix(tailwind): fixed scanned paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,9 @@
 module.exports = {
   content: [
     './app.vue',
-    './.vulmix/**/pages/*.{vue,js}',
-    './.vulmix/**/components/*.{vue,js}',
-    './.vulmix/**/layouts/*.{vue,js}',
+    './node_modules/vulmix/src/**/pages/**/*.{vue,js}',
+    './node_modules/vulmix/src/**/components/**/*.{vue,js}',
+    './node_modules/vulmix/src/**/layouts/**/*.{vue,js}',
     './pages/*.{vue,js}',
     './components/*.{vue,js}',
     './layouts/*.{vue,js}',

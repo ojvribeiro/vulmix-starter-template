65ef58fb06cdd152899b280fef4537e68e66f926
Victor Ribeiro <devjvribeiro@gmail.com>
2022-10-10 06:55:33 -0300

    fix: fixed Tailwind scan paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './app.vue',
+    // Built-in templates
     './node_modules/vulmix/src/**/pages/**/*.{vue,js}',
     './node_modules/vulmix/src/**/components/**/*.{vue,js}',
     './node_modules/vulmix/src/**/layouts/**/*.{vue,js}',
-    './pages/*.{vue,js}',
-    './components/*.{vue,js}',
-    './layouts/*.{vue,js}',
+
+    // User templates
+    './app.vue',
+    './pages/**/*.{vue,js}',
+    './components/**/*.{vue,js}',
+    './layouts/**/*.{vue,js}',
   ],
   theme: {
     extend: {},

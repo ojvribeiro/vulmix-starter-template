8fb2ad35e78dd75157e5d59469bdf6d21405ef2b
Victor Ribeiro <devjvribeiro@gmail.com>
2022-10-15 18:01:35 -0300

    feat: removed vulmix from Tailwind scan paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    // Built-in templates
-    './node_modules/vulmix/src/**/pages/**/*.{vue,js}',
-    './node_modules/vulmix/src/**/components/**/*.{vue,js}',
-    './node_modules/vulmix/src/**/layouts/**/*.{vue,js}',
-
-    // User templates
     './app.vue',
     './pages/**/*.{vue,js}',
     './components/**/*.{vue,js}',
